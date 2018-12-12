3ae36b440ac5c99414fafd3ac2db75d0871c388b
tomayac <steiner.thomas@gmail.com>
2018-12-12 09:10:10 +0100

    Prevent potential race condition with Wake Lock

--- src/js/main.js
+++ src/js/main.js
@@ -182,22 +182,22 @@
       let wakeLockObj = null;
       nav.getWakeLock('screen').then((wlObj) => {
         wakeLockObj = wlObj;
+        let wakeLockRequest = null;
+        const toggleWakeLock = () => {
+          if (wakeLockRequest) {
+            wakeLockRequest.cancel();
+            wakeLockRequest = null;
+            return;
+          }
+          wakeLockRequest = wakeLockObj.createRequest();
+        };
+        wakeLockCheckbox.addEventListener('click', () => {
+          toggleWakeLock();
+          return console.log(
+              `Wake lock is ${wakeLockObj.active ? 'active' : 'not active'}`);
+        });
       }).catch((err) => {
         return console.error('Could not obtain wake lock', err);
-      });
-      let wakeLockRequest = null;
-      const toggleWakeLock = () => {
-        if (wakeLockRequest) {
-          wakeLockRequest.cancel();
-          wakeLockRequest = null;
-          return;
-        }
-        wakeLockRequest = wakeLockObj.createRequest();
-      };
-      wakeLockCheckbox.addEventListener('click', () => {
-        toggleWakeLock();
-        return console.log(
-            `Wake lock is ${wakeLockObj.active ? 'active' : 'not active'}`);
       });
     }
   })();
